b2bd9f6d730520f574bf062f21e82ec8ec2d4db4
danielfekede1616@gmail.com <danielfekede1616@gmail.com>
2025-08-06 08:25:22 +0300

    added  scrolls components

---
+++ src/components/scrolls.jsx
@@ -0,0 +1,11 @@
+const Scrolls=()=>{
+    return(
+        <div className="flex justify-center mt-30 gap-x-10">
+            <button className="cursor-pointer size-2 rounded-full border border-black bg-transparent"></button>
+            <button className="cursor-pointer size-2 rounded-full bg-gray-200"></button>
+            <button className="cursor-pointer size-2 rounded-full border border-black bg-transparent"></button>
+        
+        </div>
+    );
+};
+export default Scrolls;
