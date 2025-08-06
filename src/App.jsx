86ca7f820217283350e99dbac3191ee7085d581b
danielfekede1616@gmail.com <danielfekede1616@gmail.com>
2025-08-06 08:30:02 +0300

    modified the componenet with hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Hero from './components/hero'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Hero/>
   )
 }
 
