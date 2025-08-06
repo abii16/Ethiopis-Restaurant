14f57c8ac0ae6e86ad6331de818a227aacad869a
danielfekede1616@gmail.com <danielfekede1616@gmail.com>
2025-08-06 08:20:07 +0300

    added button hero components

---
+++ src/components/buttonhero.jsx
@@ -0,0 +1,12 @@
+const Buttonhero=()=>{
+    return(
+        <div className=" animate-bounce flex text-center justify-center mt-15">
+            <button className="py-1 px-10 rounded-xl  bg-white/20 border border-white/30 backdrop-blur-md">
+            <a href="/" className="">Explore Our Menu</a>
+            </button>
+        </div>
+
+
+    )
+}
+export default Buttonhero;
