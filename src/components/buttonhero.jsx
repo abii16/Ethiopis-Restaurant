76ecde70eb85e0e5cd38b35e57f4e72e14f34d8d
danielfekede1616@gmail.com <danielfekede1616@gmail.com>
2025-08-07 20:41:44 +0300

    added cursor pointer

--- src/components/buttonhero.jsx
+++ src/components/buttonhero.jsx
@@ -1,7 +1,7 @@
 const Buttonhero=()=>{
     return(
-        <div className=" animate-bounce flex text-center justify-center mt-15">
-            <button className="py-1 px-10 rounded-xl  bg-white/20 border border-white/30 backdrop-blur-md">
+        <div className=" animate-bounce flex text-center justify-center mt-12 ">
+            <button className=" cursor-pointer py-1 px-10 rounded-xl  bg-white/20 border border-white/30 hover:border hover:border-black hover:bg-transparent hover:bg-none">
             <a href="/" className="">Explore Our Menu</a>
             </button>
         </div>
