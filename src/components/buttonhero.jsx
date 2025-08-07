1b60de2a1e4eeba090588396b007beeac1854335
danielfekede1616@gmail.com <danielfekede1616@gmail.com>
2025-08-07 20:52:18 +0300

    added the hover white text

--- src/components/buttonhero.jsx
+++ src/components/buttonhero.jsx
@@ -1,8 +1,8 @@
 const Buttonhero=()=>{
     return(
         <div className=" animate-bounce flex text-center justify-center mt-12 ">
-            <button className=" cursor-pointer py-1 px-10 rounded-xl  bg-white/20 border border-white/30 hover:border hover:border-black hover:bg-transparent hover:bg-none">
-            <a href="/" className="">Explore Our Menu</a>
+            <button className=" cursor-pointer py-1 px-10 rounded-xl  bg-white/20 border border-white/30 hover:border hover:border-black  hover:bg-transparent hover:bg-none">
+            <a href="/" className="hover:text-white">Explore Our Menu</a>
             </button>
         </div>
 
