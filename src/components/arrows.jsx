aa18ba053a42fc144e4ed9cc435b4029a899e696
danielfekede1616@gmail.com <danielfekede1616@gmail.com>
2025-08-07 20:38:09 +0300

    added the slide functionality with arrow

--- src/components/arrows.jsx
+++ src/components/arrows.jsx
@@ -1,17 +1,15 @@
-
 import ar from '../assets/right-arrow.png'
 import al from '../assets/left-arrow .png'
-const Arrows=()=>{
+
+const Arrows = ({ prevSlide, nextSlide }) => {
     return(
         <>
-        <div className='flex justify-between'>
-            <img src={al} alt="arr l" className='w-7 h-7 '/>
-            <img src={ar} alt="arr r" className='size-7 '/>
-        </div>
+            <div className='flex justify-between'>
+                <img src={al} alt="arr l" onClick={prevSlide} className='cursor-pointer px-3 w-9 h-7 active:bg-white/50 active:animate-ping active:rounded-full' />
+                <img src={ar} alt="arr r" onClick={nextSlide} className='cursor-pointer px-3 w-9 h-7 active:bg-white/50 active:animate-ping active:rounded-full' />
+            </div>
         </>
-
-
-
     )
 }
-export default Arrows
+
+export default Arrows;
